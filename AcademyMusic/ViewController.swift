5fd6d14b3257a0863984e97366a7de0bcd657506
João Brentano <joaobrentano.br@gmail.com>
2021-06-18 13:54:10 -0300

    Initial Commit

---
+++ AcademyMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AcademyMusic
+//
+//  Created by João Brentano on 18/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
