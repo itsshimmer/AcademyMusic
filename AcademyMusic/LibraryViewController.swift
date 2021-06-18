523eb0138ee7ea0e310ecd1db6b6ed9293a82870
Lucas Dimer Justo <lucas.dimer.justo@hotmail.com>
2021-06-18 15:20:02 -0300

    Initial Structure done, now each member should take one storyboard to proceed.

---
+++ AcademyMusic/LibraryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LibraryViewController.swift
+//  AcademyMusic
+//
+//  Created by Lucas Dimer Justo on 18/06/21.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.largeContentTitle = "library"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
