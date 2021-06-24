50e5b62d983f14da8c774ba5068f3c24d22f31ca
lccxm <>
2021-06-24 15:20:44 -0300

    working slider on PlayingStoryboard

--- AcademyMusic/PlayingViewController.swift
+++ AcademyMusic/PlayingViewController.swift
@@ -11,6 +11,7 @@ class PlayingViewController: UIViewController {
     
     private var musicService: MusicService = try! MusicService()
     var song: Music? = nil
+    var isPlaying: Bool = true
     
     @IBOutlet weak var songImage: UIImageView!
     @IBOutlet weak var songTitle: UILabel!
@@ -21,16 +22,34 @@ class PlayingViewController: UIViewController {
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var likeButton: UIButton!
+    
+    let formatter = DateComponentsFormatter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        formatter.allowedUnits = [.minute, .second]
+        
+        song = musicService.favoriteMusics[0]
         guard let song = song else { return }
         songImage.image = musicService.getCoverImage(forItemIded: song.id)
         songTitle.text = song.title
         artist.text = song.artist
-        
+        slider.minimumValue = 0
+        slider.maximumValue = Float(song.length)
+        timePassed.text = formatter.string(from: TimeInterval(slider.value))
+        remainingTime.text = formatter.string(from: song.length)
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
     }
     
-
+    @IBAction func sliderAction(_ sender: UISlider) {
+        timePassed.text = formatter.string(from: TimeInterval(slider.value))
+        remainingTime.text = formatter.string(from: (Double(slider.value) - song!.length))
+    }
+    
+    @objc func fireTimer() {
+        slider.value += 1
+        sliderAction(slider)
+    }
+    
 }
