a0d536192e3a52988c5646d27c2bd7187d02100a
lccxm <>
2021-06-24 17:32:08 -0300

    working buttons and slider on playingStoryboard

--- AcademyMusic/PlayingViewController.swift
+++ AcademyMusic/PlayingViewController.swift
@@ -25,11 +25,11 @@ class PlayingViewController: UIViewController {
     @IBOutlet weak var likeButton: UIButton!
     
     let formatter = DateComponentsFormatter()
+    var timer: Timer? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         formatter.allowedUnits = [.minute, .second]
-        
         song = musicService.favoriteMusics[0]
         guard let song = song else { return }
         songImage.image = musicService.getCoverImage(forItemIded: song.id)
@@ -39,13 +39,54 @@ class PlayingViewController: UIViewController {
         slider.maximumValue = Float(song.length)
         timePassed.text = formatter.string(from: TimeInterval(slider.value))
         remainingTime.text = formatter.string(from: song.length)
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+        if isPlaying {
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+        }
+        musicService.favoriteMusics.contains(song) ? likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal) : likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+        isPlaying ? playButton.setImage(UIImage(systemName: "pause.circle.fill"), for: .normal) : playButton.setImage(UIImage(systemName: "play.circle.fill"), for: .normal)
+        
     }
     
     @IBAction func sliderAction(_ sender: UISlider) {
         timePassed.text = formatter.string(from: TimeInterval(slider.value))
         remainingTime.text = formatter.string(from: (Double(slider.value) - song!.length))
     }
+    
+    @IBAction func backButtonAction(_ sender: UIButton) {
+        slider.value = 0
+        sliderAction(slider)
+    }
+    
+    @IBAction func nextButtonAction(_ sender: UIButton) {
+        if !musicService.queue.nextInCollection.isEmpty {
+            song = musicService.queue.nextInCollection.first
+            viewDidLoad()
+        }
+    }
+    
+    @IBAction func playButtonAction(_ sender: UIButton) {
+        if isPlaying {
+            isPlaying = false
+            timer?.invalidate()
+            playButton.setImage(UIImage(systemName: "play.circle.fill"), for: .normal)
+        } else {
+            isPlaying = true
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+            playButton.setImage(UIImage(systemName: "pause.circle.fill"), for: .normal)
+        }
+    }
+    
+    @IBAction func likeButtonAction(_ sender: UIButton) {
+        guard let song = song else { return }
+        if musicService.favoriteMusics.contains(song) {
+            musicService.toggleFavorite(music: song, isFavorite: false)
+            likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+        } else {
+            musicService.toggleFavorite(music: song, isFavorite: true)
+            likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+        }
+    }
+    
     
     @objc func fireTimer() {
         slider.value += 1
